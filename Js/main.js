8c0785646c75054b2febb7b8a18621fd1de43b7f
almapazavani <avani.thakkar@gatewaytechnolabs.com>
2015-09-28 12:34:32 +0530

    Angular Js: Nested View

---
+++ Js/main.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module('routerApp', ['ui.router']);
+app.config(function ($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/state1");
+    //
+    // Now set up the states
+    $stateProvider
+      .state('state1', {
+          url: "/state1",
+          templateUrl: "state1.html"
+      })
+      .state('state2', {
+          url: "/state2",
+          templateUrl: "state2.html"
+      });
+});
+
